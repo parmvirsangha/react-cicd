0b97b847f1cea821ca8bca57bca9fc3ab743b470
parmvirsangha <sanghaparm@outlook.com>
2025-04-14 21:23:26 -0700

    Fix GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Base path for GitHub Pages deployment
+  base: '/react-cicd/',
+
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+});
